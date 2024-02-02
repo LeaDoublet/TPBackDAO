479202d81ea435446246aada4a5b03dbd5bcbe6b
Léa Doublet <leadbtcode@gmail.com>
2024-02-02 08:37:21 +0100

    Update CountryRepository.java

--- src/main/java/monprojet/dao/CountryRepository.java
+++ src/main/java/monprojet/dao/CountryRepository.java
@@ -7,9 +7,19 @@ import org.springframework.data.jpa.repository.Query;
 
 import monprojet.entity.City;
 import monprojet.entity.Country;
+import org.springframework.data.repository.query.Param;
 
 // This will be AUTO IMPLEMENTED by Spring 
 
 public interface CountryRepository extends JpaRepository<Country, Integer> {
 
+
+    /*@Query(value = "SELECT SUM(city.population) FROM Country country JOIN country.cities city WHERE country.id = ?1")
+    Integer calculatePopulationByCountryId(Integer countryId);
+
+    @Query(value = "SELECT country.name, SUM(city.population) FROM Country country JOIN country.cities city GROUP BY country")
+    List<Object[]> getCountryPopulation(); */
+
+    @Query(value = "SELECT SUM(city.population) FROM City city WHERE city.country.id = :countryId")
+    Integer calculatePopulationByCountryId2(@Param("countryId") Integer countryId);
 }
